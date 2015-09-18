c4c7e07b38d00f9bf2a64fca35ab2b00827d84dd
Hugo Villain <hugoeland@MacBook-Pro-de-Hugo-2.local>
2015-09-18 20:04:33 +0200

    fonctions sont pretes

--- index.js
+++ index.js
@@ -21,6 +21,50 @@ app.get('/getMyFormBack/:token', function(req,res){
 	});
 });
 
+app.post('/postDiagnostic', function(req,res){
+	var async = false;
+	var ip_pool = "Main Pool";
+	var send_at = "example send_at";
+	var message = {
+		"html": "Hello Jab !",
+		"text": "Nouveau prospect",
+		"subject": "Livre blanc",
+		"from_email": "hvillain@student.42.fr",
+		"from_name": req.body.lname + ' ' + req.body.fname,
+		"to": [{
+		        "email": "hvillain@student.42.fr",
+		        "name": "pour Jab",
+		        "type": "to"
+		    }],
+		"headers": {
+		    "Reply-To": req.body.email
+		},
+		"important": false,
+		"track_opens": null,
+		"track_clicks": null,
+		"auto_text": null,
+		"auto_html": null,
+		"inline_css": null,
+		"url_strip_qs": null,
+		"preserve_recipients": null,
+		"view_content_link": null,
+		"bcc_address": "",
+		"tracking_domain": null,
+		"signing_domain": null,
+		"return_path_domain": null,
+		"merge": true,
+	"merge_language": "mailchimp"
+	};
+
+	mandrill_client.messages.send({"message": message, "async": async, "ip_pool": ip_pool, "send_at": ''}, function(result) {
+	    res.send(200, {message:'Merci! Vous allez recevoir sous peu un lien de téléchargement dans votre boîte mail.\nVous êtes éligible à la disruption? Passez le test!'});
+	}, function(e) {
+	    console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+	    res.send(406, e);
+	    return res.redirect('/');
+	});
+});
+
 app.get('/*', function(request, response) {
   response.sendFile(__dirname + '/app/dist/index.html');
   //response.redirect('/');
@@ -29,5 +73,4 @@ app.get('/*', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
 
