cacdafb38e78948d641b7016e85d2a6b0b89ac81
Hugo Villain <hugoeland@MacBook-Pro-de-Hugo-2.local>
2015-09-23 17:37:46 +0200

    postCOntact

--- index.js
+++ index.js
@@ -31,43 +31,80 @@ app.post('/postDiagnostic', function(req,res){
 	var async = false;
 	var ip_pool = "Main Pool";
 	var send_at = "example send_at";
-	console.log(req);
-	var message = {
-		"html": "Hello Jab !<br>Vous avez reçu un nouveau lead.<br><br>\
-		Son identité : " + req.body.fname + " " + req.body.lname + "<br>\
-		Son numéro de téléphone: " + req.body.phone + "<br>\
-		Son adresse email: " + req.body.email + "<br>\
-		Son secteur d\'activité: " + req.body.sector + "<br>\
-		Son entreprise: " + req.body.company + "\
-		<br><br>Voici son questionnaire finalisé : <br><br>",
-		
-		"subject": "Un nouveau prospect pour EDR",
-		"from_email": "hvillain@student.42.fr",
-		"from_name": req.body.lname + ' ' + req.body.fname,
-		"to": [{
-		        "email": "hvillain@student.42.fr",
-		        "name": "pour Jab",
-		        "type": "to"
-		    }],
-		"headers": {
-		    "Reply-To": req.body.email
-		},
-		"important": false,
-		"track_opens": null,
-		"track_clicks": null,
-		"auto_text": null,
-		"auto_html": null,
-		"inline_css": null,
-		"url_strip_qs": null,
-		"preserve_recipients": null,
-		"view_content_link": null,
-		"bcc_address": "",
-		"tracking_domain": null,
-		"signing_domain": null,
-		"return_path_domain": null,
-		"merge": true,
-	"merge_language": "mailchimp"
-	};
+	console.log(req.body.contact);
+	if (!req.body.contact || req.body.contact == false){
+		var message = {
+			"html": "Hello Jab !<br>Vous avez reçu un nouveau lead.<br><br>\
+			Son identité : " + req.body.fname + " " + req.body.lname + "<br>\
+			Son numéro de téléphone: " + req.body.phone + "<br>\
+			Son adresse email: " + req.body.email + "<br>\
+			Son secteur d\'activité: " + req.body.sector + "<br>\
+			Son entreprise: " + req.body.company + "\
+			<br><br>Voici son questionnaire finalisé : <br><br>",
+			
+			"subject": "Un nouveau prospect pour EDR",
+			"from_email": "hvillain@student.42.fr",
+			"from_name": req.body.lname + ' ' + req.body.fname,
+			"to": [{
+			        "email": "hvillain@student.42.fr",
+			        "name": "pour Jab",
+			        "type": "to"
+			    }],
+			"headers": {
+			    "Reply-To": req.body.email
+			},
+			"important": false,
+			"track_opens": null,
+			"track_clicks": null,
+			"auto_text": null,
+			"auto_html": null,
+			"inline_css": null,
+			"url_strip_qs": null,
+			"preserve_recipients": null,
+			"view_content_link": null,
+			"bcc_address": "",
+			"tracking_domain": null,
+			"signing_domain": null,
+			"return_path_domain": null,
+			"merge": true,
+		"merge_language": "mailchimp"
+		};
+	}
+	else {
+		var message = {
+			"html": "Hello Jab !<br>Vous avez reçu un nouveau message.<br><br>\
+			Son entreprise: " + req.body.company +  "<br>\
+			Son entreprise: " + req.body.company + "\
+			<br><br>Voici son message : <br><br>" + req.body.message,
+			
+			"subject": "Un nouveau prospect pour EDR",
+			"from_email": "hvillain@student.42.fr",
+			"from_name": req.body.email,
+			"to": [{
+			        "email": "hvillain@student.42.fr",
+			        "name": "pour Jab",
+			        "type": "to"
+			    }],
+			"headers": {
+			    "Reply-To": req.body.email
+			},
+			"important": false,
+			"track_opens": null,
+			"track_clicks": null,
+			"auto_text": null,
+			"auto_html": null,
+			"inline_css": null,
+			"url_strip_qs": null,
+			"preserve_recipients": null,
+			"view_content_link": null,
+			"bcc_address": "",
+			"tracking_domain": null,
+			"signing_domain": null,
+			"return_path_domain": null,
+			"merge": true,
+		"merge_language": "mailchimp"
+		};		
+	}
 
 	mandrill_client.messages.send({"message": message, "async": async, "ip_pool": ip_pool, "send_at": ''}, function(result) {
 	    res.send(200);
